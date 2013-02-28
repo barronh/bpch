0d6e362a4ae08031e3b19814c2485f7109e9c572
Barron Henderson <barronh@gmail.com>
2013-02-28 01:22:42 -0500

    Add install script

---
+++ setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(name = 'bpch',
+      version = '1.0rc',
+      author = 'Barron Henderson',
+      author_email = 'barronh@ufl.edu',
+      maintainer = 'Barron Henderson',
+      maintainer_email = 'barronh@ufl.edu',
+      py_modules = ['bpch'],
+      requires = ['numpy (>=1.5)', 'matplotlib (>=1.0)'],
+      url = 'https://github.com/barronh/bpch'
+      )
