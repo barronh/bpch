5c4ea47ee4b7fd4c90a2b079f6d7e5a67ccc1a33
Barron Henderson <barronh@gmail.com>
2014-01-10 16:48:59 -0500

    fixing predicate

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name = 'bpch',
       maintainer = 'Barron Henderson',
       maintainer_email = 'barronh@ufl.edu',
       py_modules = ['bpch'],
-      install_requires = ['numpy >=1.5', 'matplotlib >=1.0'],
-      requires = ['numpy >=1.5', 'matplotlib >=1.0'],
+      install_requires = ['numpy (>=1.5)', 'matplotlib (>=1.0)'],
+      requires = ['numpy (>=1.5)', 'matplotlib (>=1.0)'],
       url = 'https://github.com/barronh/bpch',
       download_url = 'https://github.com/barronh/bpch/archive/1.0rc.zip'
       )
