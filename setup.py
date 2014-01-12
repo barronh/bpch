c8a9ac57b8b2c4fea3a491432d08470ac6884e44
Barron Henderson <barronh@gmail.com>
2014-01-12 10:33:01 -0500

    Updating version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ except:
     from distutils.core import setup
 
 setup(name = 'bpch',
-      version = '1.0rc',
+      version = '1.0',
       author = 'Barron Henderson',
       author_email = 'barronh@ufl.edu',
       maintainer = 'Barron Henderson',
       maintainer_email = 'barronh@ufl.edu',
       py_modules = ['bpch'],
       requires = ['numpy (>=1.5)', 'matplotlib (>=1.0)'],
-      url = 'https://github.com/barronh/bpch',
-      download_url = 'https://github.com/barronh/bpch/archive/1.0.zip'
+      url = 'http://github.com/barronh/bpch',
+      download_url = 'http://github.com/barronh/bpch/archive/1.0.zip'
       )
